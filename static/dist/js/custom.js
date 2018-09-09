80fb3b5e546fa51cebfcc280e207ac40df0d261b
NemanjaMandic <nemanja.mandic@folkmatic.com>
2018-09-09 22:17:41 +0200

    added responsive images

--- static/dist/js/custom.js
+++ static/dist/js/custom.js
@@ -1,12 +1,14 @@
+window.onload = function () {
+	var logo = document.getElementById("closeSmallMenu");
+	var mainNavSm = document.getElementById("mainNavSm");
 
-window.onload = function() {
-  var logo = document.getElementById("closeSmallMenu");
-  var mainNavSm = document.getElementById("mainNavSm");
-  
 
-  logo.onclick = function(){
-  	TweenLite.to(mainNavSm, 1, {x:"542px", opacity: 0});
-  }
+	logo.onclick = function () {
+		TweenLite.to(mainNavSm, 1, {
+			x: "542px",
+			opacity: 0
+		});
+	}
 }
 
 // var panel1 = new TimelineLite();
@@ -20,32 +22,32 @@ window.onload = function() {
 // var panel3 = new TimelineLite();
 // panel3.from(...);
 //   ...
-function toggleElementWithId(id){
-	$("#"+id).slideToggle('slow');
+function toggleElementWithId(id) {
+	$("#" + id).slideToggle('slow');
 }
 
-function toggleElementWithClass(className){
-	$("."+className).slideToggle('slow');
+function toggleElementWithClass(className) {
+	$("." + className).slideToggle('slow');
 }
 
-function openWithId(id){
-	$("#"+id).slideDown("slow");
+function openWithId(id) {
+	$("#" + id).slideDown("slow");
 }
 
-function closeWithId(id){
-	$("#"+id).slideUp("slow");
+function closeWithId(id) {
+	$("#" + id).slideUp("slow");
 }
 
 
-function toggleMobileMenu(){
-		
-	$("#toggleTopNav").on('click', function(e){
+function toggleMobileMenu() {
+
+	$("#toggleTopNav").on('click', function (e) {
 		e.preventDefault();
 		$(this).toggleClass("rotate");
 		toggleElementWithId('firstNav');
 	});
 
-	$("#toggleBottomNav").on('click', function(e){
+	$("#toggleBottomNav").on('click', function (e) {
 		e.preventDefault();
 		$(this).toggleClass("rotate");
 		toggleElementWithId('secondNav');
@@ -53,72 +55,314 @@ function toggleMobileMenu(){
 }
 
 
-function openContactForm(){
-	$("#openContact").on('click', function(e){
+function openContactForm() {
+	$("#openContact").on('click', function (e) {
 		e.preventDefault();
 		openWithId("contactContent");
 	});
 }
 
-function closeContactForm(){
+function closeContactForm() {
 	//closeWithId("closeContact");
-	$("#closeContact").on('click', function(e){
+	$("#closeContact").on('click', function (e) {
 		e.preventDefault();
 		closeWithId("contactContent");
 	});
 }
 
-$(document).ready(function(){
-console.log( "E Dzesi ddd ffggf");
 
-//call functions
-toggleMobileMenu();
-openContactForm();
-closeContactForm();
+$(document).ready(function () {
 
 
+	//call functions
+	toggleMobileMenu();
+	openContactForm();
+	closeContactForm();
+
+	//numberCounter();
+
+	var controller = new ScrollMagic.Controller();
+
+
+	var options = {  
+		useEasing: true,
+		  useGrouping: true,
+		  separator: '',
+		  decimal: '',
+	};
+
+	var netoOptions = {  
+		useEasing: true,
+		  useGrouping: true,
+		  separator: '',
+		  decimal: '',
+		suffix: 'M€'
+	};
+	var ugovoriCount = new CountUp('ugovoriElement', 0, 3500, 0, 2.5, options);
+
+
+	var ugovori2Count = new CountUp('ugovori2Element', 0, 7345, 0, 2.5, options);
+
+
+	var netoCount = new CountUp('netoElement', 0, 22, 0, 2.5, netoOptions);
+
+	var tl = new TimelineMax();
+
+	//var tween = TweenMax.to($(this), 0.5, {y: '+=100', ease:Linear.easeNone});
+
+	//build a scene
+	var scene = new ScrollMagic.Scene({
+
+			triggerElement: '.about-module',
+			duration: '300',
+			//triggerHook: 0
+
+		}).on('enter', function () {
+			if (!ugovoriCount.error) {
+				ugovoriCount.start();
+			} else {  
+				console.error(ugovoriCount.error);
+			}
+
+			if (!ugovori2Count.error) {
+				ugovori2Count.start();
+			} else {  
+				console.error(ugovori2Count.error);
+			}
+
+			if (!netoCount.error) {
+				netoCount.start();
+			} else {  
+				console.error(netoCount.error);
+			}
+		})
+		//.setClassToggle(this, 'do-stuff')
+
+		.addIndicators({
+			name: "animate box scene",
+			colorEnd: 'red',
+		})
+		.addTo(controller);
+
+	/////info module
+	var infoTween = new TimelineMax();
+	infoTween.fromTo("#infoOverlay", 0.5, {
+			width: '0%'
+		}, {
+			width: '100%',
+			yoyo: true,
+			ease: Circ.easeInOut
+		})
+
+		.to("#infoOverlay", 2, {
+			x: "100%",
+			yoyo: true,
+			ease: Circ.easeInOut
+		})
+		.to("#infoOverlay", 0, {
+			width: "0%"
+		})
+
+
+	var infoScene = new ScrollMagic.Scene({
+			triggerElement: '.info-module',
+			//duration: '500',
+			reverse: false
+		})
+		.setTween(infoTween)
+		.addIndicators({
+			name: "info scene",
+			colorEnd: 'red',
+		})
+		.addTo(controller);
+
+	/////info module
+	var infoBoxTween = new TimelineMax();
+
+	infoBoxTween.from('.info-content', 0.5, {
+			y: 80,
+			opacity: 0,
+			ease: Power4.easeOut
+		})
+		.from('#infoBox1', 0.2, {
+			y: 10,
+			opacity: 0,
+			ease: Power4.easeOut
+		})
+		.from('#infoBox2', 0.2, {
+			y: 10,
+			opacity: 0,
+			ease: Power4.easeOut
+		})
+		.from('#infoBox3', 0.2, {
+			y: 10,
+			opacity: 0,
+			ease: Power4.easeOut
+		})
+		.from('.cta-wrapper', 0.5, {
+			y: 10,
+			opacity: 0,
+			ease: Power4.easeOut
+		})
+
+
+	var infoBoxScene = new ScrollMagic.Scene({
+			triggerElement: '.box-container-animate',
+			//duration: '200',
+			reverse: false
+		})
+		.setTween(infoBoxTween)
+		.addIndicators({
+			name: "box scene",
+			colorEnd: 'red',
+		})
+		.addTo(controller);
+
+
+	//blog scene
+	var blogTween = new TimelineMax();
+
+	blogTween.from('#blog1', 0.2, {
+			y: 8,
+			opacity: 0,
+			ease: Power4.easeOut
+		})
+		.from('#blog2', 0.2, {
+			y: 8,
+			opacity: 0,
+			ease: Power4.easeOut
+		})
+		.from('#blog3', 0.2, {
+			y: 8,
+			opacity: 0,
+			ease: Power4.easeOut
+		})
+
+	var blogScene = new ScrollMagic.Scene({
+			triggerElement: '.blog-module',
+			//duration: '500',
+			reverse: false
+		})
+		.setTween(blogTween)
+		.addIndicators({
+			name: "blog scene",
+			colorEnd: 'red',
+		})
+		.addTo(controller);
 });
-$(document).ready(function(){
-  
+$(document).ready(function () {
+
   var $window = $(window);
 
-  function isLargeScreen(){
-  	var windowsize = $window.width();
-  	if(windowsize > 768){
-  		return true;
-  	}
+  function isLargeScreen() {
+    var windowsize = $window.width();
+    if (windowsize > 768) {
+      return true;
+    }
   }
-  var aboutGlide = new Glide('.about-glide',{
-	type: 'carousel',
-	perView: 3,
-  dragThreshold: false,
-  swipeThreshold: false,
-  autoplay: false,
-  //focusAt: 'center',
-	breakpoints: {
+  var aboutGlide = new Glide('.about-glide', {
+      type: 'carousel',
+      perView: 3,
+      dragThreshold: false,
+      swipeThreshold: false,
+      autoplay: false,
+      //focusAt: 'center',
+      breakpoints: {
 
-		768: {
-			perView: 1,
-      dragThreshold: 120,
-      swipeThreshold: 80,
-			autoplay: 3000,
-			animationDuration: 1000,
-      animationTimingFunc: 'ease-in-out'
-           
-		}
-	}
-	
-  }
-  		
+        768: {
+          perView: 1,
+          dragThreshold: 120,
+          swipeThreshold: 80,
+          autoplay: 3000,
+          animationDuration: 1000,
+          animationTimingFunc: 'ease-in-out'
+
+        }
+      }
+
+    }
+
   );
 
-  if(isLargeScreen()){
-  	aboutGlide.disable();
+  if (isLargeScreen()) {
+    aboutGlide.disable();
   }
   aboutGlide.mount();
 
-  new Glide('.glide-desktop-header', {
-  	animationDuration: 1000,
-  	animationTimingFunc: 'ease-in-out'
-  }).mount();
+  var desktopGlide = new Glide('.glide-desktop-header', {
+    type: 'carousel',
+    animationDuration: 1000,
+    animationTimingFunc: 'ease-in-out'
+  });
+
+  //   desktopGlide.on("build.after", function(event, data) {
+  //     // Access to event data
+  //     console.log('Resajzuje se ', data);
+  //     TweenMax.fromTo("#glideContent", 0.5, {y: '0', autoAlpha:0 }, {y: '-100', autoAlpha:1 });
+  // });
+
+  var tlHeader = new TimelineMax();
+
+  tlHeader.set("#gcLabel", {
+    opacity: 0
+  });
+  tlHeader.set("#gcTitle", {
+    opacity: 0
+  });
+  tlHeader.set("#gcText", {
+    opacity: 0
+  });
+  tlHeader.set("#gcLink", {
+    opacity: 0
+  });
+
+  //tlHeader.set("#gcLabel",{autoAlpha:0});
+
+  desktopGlide.on("move.after", function (event, data) {
+    // Access to event data
+
+
+    tlHeader.set("#gcLabel", {
+      opacity: 0
+    });
+    tlHeader.set("#gcTitle", {
+      opacity: 0
+    });
+    tlHeader.set("#gcText", {
+      opacity: 0
+    });
+    tlHeader.set("#gcLink", {
+      opacity: 0
+    });
+    tlHeader.staggerFromTo("#gcLabel", 0.5, {
+      opacity: 0,
+      y: '19'
+    }, {
+      opacity: 1,
+      y: '0'
+    });
+    tlHeader.staggerFromTo("#gcTitle", 0.5, {
+      opacity: 0,
+      y: '19'
+    }, {
+      opacity: 1,
+      y: '0'
+    });
+    tlHeader.staggerFromTo("#gcText", 0.5, {
+      y: '19',
+      opacity: 0
+    }, {
+      y: '0',
+      opacity: 1
+    });
+    tlHeader.staggerFromTo("#gcLink", 0.5, {
+      y: '19',
+      opacity: 0
+    }, {
+      y: '0',
+      opacity: 1
+    });
+  });
+
+  desktopGlide.mount();
 });
--- static/js/sliders.js
+++ static/js/sliders.js
@@ -1,44 +1,116 @@
-$(document).ready(function(){
-  
+$(document).ready(function () {
+
   var $window = $(window);
 
-  function isLargeScreen(){
-  	var windowsize = $window.width();
-  	if(windowsize > 768){
-  		return true;
-  	}
+  function isLargeScreen() {
+    var windowsize = $window.width();
+    if (windowsize > 768) {
+      return true;
+    }
   }
-  var aboutGlide = new Glide('.about-glide',{
-	type: 'carousel',
-	perView: 3,
-  dragThreshold: false,
-  swipeThreshold: false,
-  autoplay: false,
-  //focusAt: 'center',
-	breakpoints: {
+  var aboutGlide = new Glide('.about-glide', {
+      type: 'carousel',
+      perView: 3,
+      dragThreshold: false,
+      swipeThreshold: false,
+      autoplay: false,
+      //focusAt: 'center',
+      breakpoints: {
 
-		768: {
-			perView: 1,
-      dragThreshold: 120,
-      swipeThreshold: 80,
-			autoplay: 3000,
-			animationDuration: 1000,
-      animationTimingFunc: 'ease-in-out'
-           
-		}
-	}
-	
-  }
-  		
+        768: {
+          perView: 1,
+          dragThreshold: 120,
+          swipeThreshold: 80,
+          autoplay: 3000,
+          animationDuration: 1000,
+          animationTimingFunc: 'ease-in-out'
+
+        }
+      }
+
+    }
+
   );
 
-  if(isLargeScreen()){
-  	aboutGlide.disable();
+  if (isLargeScreen()) {
+    aboutGlide.disable();
   }
   aboutGlide.mount();
 
-  new Glide('.glide-desktop-header', {
-  	animationDuration: 1000,
-  	animationTimingFunc: 'ease-in-out'
-  }).mount();
+  var desktopGlide = new Glide('.glide-desktop-header', {
+    type: 'carousel',
+    animationDuration: 1000,
+    animationTimingFunc: 'ease-in-out'
+  });
+
+  //   desktopGlide.on("build.after", function(event, data) {
+  //     // Access to event data
+  //     console.log('Resajzuje se ', data);
+  //     TweenMax.fromTo("#glideContent", 0.5, {y: '0', autoAlpha:0 }, {y: '-100', autoAlpha:1 });
+  // });
+
+  var tlHeader = new TimelineMax();
+
+  tlHeader.set("#gcLabel", {
+    opacity: 0
+  });
+  tlHeader.set("#gcTitle", {
+    opacity: 0
+  });
+  tlHeader.set("#gcText", {
+    opacity: 0
+  });
+  tlHeader.set("#gcLink", {
+    opacity: 0
+  });
+
+  //tlHeader.set("#gcLabel",{autoAlpha:0});
+
+  desktopGlide.on("move.after", function (event, data) {
+    // Access to event data
+
+
+    tlHeader.set("#gcLabel", {
+      opacity: 0
+    });
+    tlHeader.set("#gcTitle", {
+      opacity: 0
+    });
+    tlHeader.set("#gcText", {
+      opacity: 0
+    });
+    tlHeader.set("#gcLink", {
+      opacity: 0
+    });
+    tlHeader.staggerFromTo("#gcLabel", 0.5, {
+      opacity: 0,
+      y: '19'
+    }, {
+      opacity: 1,
+      y: '0'
+    });
+    tlHeader.staggerFromTo("#gcTitle", 0.5, {
+      opacity: 0,
+      y: '19'
+    }, {
+      opacity: 1,
+      y: '0'
+    });
+    tlHeader.staggerFromTo("#gcText", 0.5, {
+      y: '19',
+      opacity: 0
+    }, {
+      y: '0',
+      opacity: 1
+    });
+    tlHeader.staggerFromTo("#gcLink", 0.5, {
+      y: '19',
+      opacity: 0
+    }, {
+      y: '0',
+      opacity: 1
+    });
+  });
+
+  desktopGlide.mount();
 });
